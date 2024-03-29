f72f10a8bc262842ca33932cb636b813c719872c
mdfaysalbabu <foysalfoysal313@gmail.com>
2024-03-29 16:08:51 +0600

    Readme Md

--- src/app/middleWare/validateRequest.ts
+++ src/app/middleWare/validateRequest.ts
@@ -1,34 +1,16 @@
-// validateRequest.ts
 import { NextFunction, Request, Response } from "express";
-import { AnyZodObject, ZodError } from "zod";
+import { AnyZodObject } from "zod";
 
 const validateRequest =
   (schema: AnyZodObject) =>
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-      schema.parse({
+      await schema.parseAsync({
         body: req.body,
-        
       });
-      next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        // Construct a detailed error message
-        const errorDetails = error.errors.map((err) => ({
-          field: err.path.join("."),
-          message: err.message,
-        }));
-
-        res.status(400).json({
-          success: false,
-          message: "Validation error",
-          errorDetails: {
-            issues: errorDetails,
-          },
-        });
-      } else {
-        next(error);
-      }
+      return next();
+    } catch (err) {
+      next(err);
     }
   };
 
